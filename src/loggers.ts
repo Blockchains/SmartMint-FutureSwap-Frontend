a739662ea51b3793ce5b6b01973cf6e0d68e6676
JesseAbram <jesseabramowitz@hotmail.com>
2020-04-13 13:36:20 -0400

    token pool naming fix

--- src/loggers.ts
+++ src/loggers.ts
@@ -1,5 +1,5 @@
 import { Futureswap, TradeOpen, TradeClose, TradeLiquidate, AddCollateral, FrontRunning, UpdateLiquidity, InternalExchange } from '../generated/Futureswap/Futureswap'
-import { Trade, Liquidation, Collateral, FrontRunningCase, LiquidityAddition, Balancer, TradeWithCollateral, OpenTrade, CloseTrade, DyanmicFunding, TokenPools } from '../generated/schema'
+import { Trade, Liquidation, Collateral, FrontRunningCase, LiquidityAddition, Balancer, TradeWithCollateral, OpenTrade, CloseTrade, DyanmicFunding, TokenPool } from '../generated/schema'
 import { BigIntEth } from './helpers'
 import { returnTradesInfo, returnDynamicFunding, returnTokenPools } from './getters'
 import { Bytes, Address, BigInt } from '@graphprotocol/graph-ts'
@@ -20,7 +20,7 @@ export function logDFR (transactionHash: Bytes, address: Address, timestamp: Big
 
 export function logTokenPools(transactionHash: Bytes, address: Address, timestamp: BigInt, reason: string): void {
     let id = transactionHash.toHex()
-    let tokenPools = new TokenPools(id)
+    let tokenPools = new TokenPool(id)
     let returnData = returnTokenPools(address)
     tokenPools.exchange = address
     tokenPools.timestamp = timestamp.toI32()
